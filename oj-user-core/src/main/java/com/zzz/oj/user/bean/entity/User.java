ba436e1655d1027fd05855006841dff3a9abc340
zhizhao.zhang <zhizhao.zhang@advance.ai>
2019-04-04 04:17:20 +0800

    change JpaEntity to OJEntity

--- oj-user-core/src/main/java/com/zzz/oj/user/bean/entity/User.java
+++ oj-user-core/src/main/java/com/zzz/oj/user/bean/entity/User.java
@@ -1,16 +1,14 @@
 package com.zzz.oj.user.bean.entity;
 
+import com.zzz.oj.core.entity.OJEntity;
 import com.zzz.oj.user.bean.enums.UserStatus;
-import com.zzz.util.jpa.entity.JpaEntity;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  * Created by zhizhao.zhang on 2019/02/04 23:54.
@@ -18,22 +16,16 @@ import javax.persistence.Id;
  */
 @Data
 @Entity
-public class User implements JpaEntity<Long> {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@EqualsAndHashCode(callSuper = false)
+public class User extends OJEntity {
     private String username;
-    private String nik;
+    private String nikName;
     private String password;
     private String accessKey;
     private String secretKey;
     @Enumerated(EnumType.STRING)
     private UserStatus status;
-    private Long createTime;
-    private Long updateTime;
-
-    @Override
-    public Long primaryKey() {
-        return id;
-    }
+    private String submitIds;
+    private String acceptIds;
+    private String email;
 }
